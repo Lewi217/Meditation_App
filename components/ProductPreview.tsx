4be4dcdcddcbb990dfffe7b098afbde5d9f169ff
lewis wanjohi <wanjohilewi7@gmail.com>
2024-07-03 23:58:21 +0300

    react native application

---
+++ components/ProductPreview.tsx
@@ -0,0 +1,13 @@
+
+import { View, Text } from "react-native";
+import React from "react";
+
+const ProductPreview = () => {
+    return (
+        <View>
+            <Text>ProductPreview</Text>
+        </View>
+    );
+};
+
+export default ProductPreview;
